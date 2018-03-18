87d702336efabbb53cbaa340a5b9bf6bd6ff8a75
Infuse <infuse@ya.ru>
2018-03-18 17:24:06 +0300

    homework #1

---
+++ script.js
@@ -0,0 +1,12 @@
+//подсчет температуры в Фаренгейтах
+
+var tempcelcium = prompt("Введите температуру в Цельсиях");
+var tempfareng=parseInt(((9/5)*tempcelcium+32));
+alert("Температура в Фаренгейтах " + tempfareng + " градусов");
+
+
+//вывод имени
+var admin = null;
+var name = 'Василий';
+admin = name;
+alert(admin); 
