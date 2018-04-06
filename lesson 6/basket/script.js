c802587586d7b94c567e2d9a1121ca70c24f256e
Infuse <infuse@ya.ru>
2018-04-06 13:08:24 +0300

    scripts

--- lesson 6/basket/script.js
+++ lesson 6/basket/script.js
@@ -7,13 +7,13 @@ table.style.width = "100%";
 table.style.padding = "10px";
 table.style.minHeight = "30px";
 
-function createTR() {
+function createList() {
     cellArr=[];
     var tr = document.createElement("tr");
     tr.style.height = "40px";
     table.appendChild(tr);
     
-    for (i=0; i<4; i++) {
+    for (i=0; i<3; i++) {
         var td = document.createElement("td");
         tr.appendChild(td);
         cellArr.push(td);
@@ -23,13 +23,20 @@ function createTR() {
         console.log(small);
         
     var title = document.getElementsByClassName("title");
-        cellArr[1].innerText = title[0].innerText;
+        cellArr[0].innerText = title[0].innerText;
     
     var price = document.getElementsByClassName("price");
-        cellArr[2].innerText = price[0].innerText;
-        cellArr[3].innerText = "Удалить";  
+        cellArr[1].innerText = price[0].innerText;
+        cellArr[2].innerText = "Удалить";  
     }
 
+/*
+var button = document.getElementsByClassName("buy-button");
+x = [];
+for (i=0; i<button.length; i++) {
+    x.push(button[i].id)
+}
+console.log(x);
+*/
 
-
-buy_1.addEventListener('click', createTR);
+button[0].addEventListener('click', createList);
--- lesson 6/gallery/script.js
+++ lesson 6/gallery/script.js
@@ -18,50 +18,15 @@ function forward() {
     arrow[0].addEventListener('click', back);
     arrow[1].addEventListener('click', forward);
 
-/*
-function imgChange() {
-    for (i=0; i<images.length; i++) {
-        var img = images[i];
-        img.src = "images/big_" + [i+1] + ".jpg";
-        console.log(img);
-        //big_picture.appendChild(img[i]);
-    }
-}
 
-*/
 var images = document.querySelectorAll(".image");
 
-/* working part
-function imgChange() {
-var img = document.getElementById("big_picture");
-    console.log(big_picture);
-    big_picture.innerHTML="<img src='images/big_2.jpg'>";
-   // img.src = "images/big_2.jpg";
-    
+x = [];
+for (i=0; i<images.length; i++){
+x.push(images[i].id);
 }
-*/
-
-/* working part2
-function imgChange() {
-        
-document.getElementById("big_picture").innerHTML="<img src='images/pic_" + [i] + ".jpg'>";
-console.log(big_picture); 
-    }
-*/
-
-//function imgChange(id) {
-   // var id_item = "pic_" + x[i];
-  //  document.getElementById(id_item).innerHTML = "<img src='images/pic_" + [i] + ".jpg'>";
-//}
-
-function imgChange() {
-    x = [];
-    for (i=0; i<images.length; i++) {
-        x.push(images[i].id);
-
-    document.getElementById("big_picture").innerHTML="<img src='images/" + x[i] + ".jpg'>";
-    }
-
+     
+    document.getElementById("big_picture").innerHTML="<img src='images/" + this.id + ".jpg'>";
     }
 
 images[0].addEventListener('click', imgChange);
